f1b933d6e5369728fca3581667d15db2ab4128c3
trueroma <48531151+trueroma@users.noreply.github.com>
2023-12-20 18:40:20 +0300

    Update constants.js
    different greetings text

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,12 @@ export const token = 'PUT_YOUR_TELEGRAM_BOT_API_TOKEN_HERE'
 
 export const IMAGES_FOLDER = 'images'
 
-export const RANDOM_IMAGES_COUNT = 7
+export const RANDOM_IMAGES_COUNT = 3
 
 export const ANTI_SPAM_DELAY = 2_000
 
-export const MEMES_DELAY = 10_000
+export const MEMES_DELAY = 25_000
 
-export const START_TEXT = 'Привет! Я бот, умею то, сё и многое другое.'
+export const START_TEXT = 'Привет! Я – мемотрон, нажимай на "Получить мемов" и я отправлю тебе 3 случайные картинки, которые могут подойти под нужную тебе ситуацию!'
 
 export const MEMES_NOT_READY_TEXT = 'Новая порция мемов для тебя ещё не готова.'
