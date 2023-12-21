f5eaf268c22faa22d9b24f095473ee35aeee72cc
a.tutunnik <a.tutunnik@corp.mail.ru>
2023-12-21 13:48:15 +0300

    :sparkles: images deck

--- src/images.js
+++ src/images.js
@@ -6,9 +6,22 @@ import { IMAGES_FOLDER, RANDOM_IMAGES_COUNT } from './constants.js'
 
 const files = readdirSync(IMAGES_FOLDER)
 
-export const getRandomImages = () => files
-  .sort(() => 0.5 - Math.random())
-  .slice(0, RANDOM_IMAGES_COUNT)
-  .map(
-    file => InputMediaBuilder.photo(new InputFile(`${IMAGES_FOLDER}/${file}`))
-  )
+let pointer = Infinity
+
+export const getRandomImages = () => {
+  if (pointer + RANDOM_IMAGES_COUNT > files.length) {
+    files.sort(() => 0.5 - Math.random())
+
+    pointer = 0
+  }
+
+  const images = files
+    .slice(pointer, pointer + RANDOM_IMAGES_COUNT)
+    .map(
+      file => InputMediaBuilder.photo(new InputFile(`${IMAGES_FOLDER}/${file}`))
+    )
+
+  pointer += RANDOM_IMAGES_COUNT
+
+  return images
+}
